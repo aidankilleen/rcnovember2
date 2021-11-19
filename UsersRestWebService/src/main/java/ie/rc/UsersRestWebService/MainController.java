d292172ef0cf58b87b8c42497d6e948023398c43
Aidan Killeen <aidan.killeen@gmail.com>
2021-11-19 10:41:55 +0000

    added front end code and ajax call to rest service

--- UsersRestWebService/src/main/java/ie/rc/UsersRestWebService/MainController.java
+++ UsersRestWebService/src/main/java/ie/rc/UsersRestWebService/MainController.java
@@ -8,16 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 	
-	public MainController() {
-		
-		System.out.println("Main controller instatiated");
-	}
-
-	@GetMapping("/home")
+	@GetMapping("/")
 	public String home(Model model) {
-		System.out.println("home() called");
 		
-		model.addAttribute("name", "aidan");
-		return "abc";
+		return "index";
 	}
 }
